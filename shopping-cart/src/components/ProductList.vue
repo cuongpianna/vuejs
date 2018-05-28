89eefc7769087345ad7f67dc7045f81627cfa4dc
cuongpianna <cuongpianna1996@gmail.com>
2018-05-28 17:11:57 +0700

    vuejs

---
+++ shopping-cart/src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<template id="">
+  <div class="">
+    <h1>Product list</h1>
+    <ul>
+      <li v-for='product in products'>{{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import shop from '@/api/shop'
+  export default {
+    data(){
+      return {
+        products : []
+      }
+    },
+
+    created(){
+      shop.getProducts(products => {
+        this.products = products
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ todo-app/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <ToDoList v-bind:todos="todos"></ToDoList>
+  </div>
+</template>
+
+<script>
+import ToDoList from './components/ToDoList'
+export default{
+  name : 'app',
+  components: {
+    ToDoList,
+  },
+  data(){
+    return {
+        todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
